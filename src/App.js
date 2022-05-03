22aff225a0133fa654c2d3e49735e4668a4cde22
supriya kumari <supriyasupriya5645@gmail.com>
2022-05-03 10:33:39 +0530

    github-finer project completed

--- src/App.js
+++ src/App.js
@@ -9,30 +9,37 @@ import {GithubProvider} from './context/github/GithubContext'
 import {AlertProvider} from './context/alert/AlertContext'
 import User from './components/layout/users/User'
 
+
 function App() {
-  return (  
+  return (
     <GithubProvider>
       <AlertProvider>
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar />
+        <Router>
+          <div className='flex flex-col justify-between h-screen'>
+            <Navbar />
 
-        <main className='container mx-auto px-3 pb-12'>
-          <Alert/>
-          
-        <Routes>
-                <Route path='/' element={<Home />} />
+            <main className='container mx-auto px-3 pb-12'>
+              <Routes>
+                <Route
+                  path='/'
+                  element={
+                    <>
+                      <Alert />
+                      <Home />
+                    </>
+                  }
+                />
                 <Route path='/about' element={<About />} />
                 <Route path='/user/:login' element={<User />} />
                 <Route path='/notfound' element={<NotFound />} />
                 <Route path='*' element={<NotFound />} />
               </Routes>
-        </main>
+            </main>
 
-        <Footer />
-      </div>
-    </Router>
-    </AlertProvider>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   )
 }
--- src/components/layout/users/UserResults.jsx
+++ src/components/layout/users/UserResults.jsx
@@ -7,26 +7,17 @@ import GithubContext from '../../../context/github/GithubContext'
 function UserResults() {
   const { users, loading } = useContext(GithubContext)
 
-  // useEffect(()=>{
-  // //  fetchUsers()
-
-  // }, [])
-
-  
-  if(!loading){
+  if (!loading) {
     return (
-
-
-      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3
-      md:grid-cols-2">{users.map((users)=>(
-        
-       <UserItem key={users.id} user={users}/>
-      ))} </div>
+      <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
     )
-  } else{
-   return <Spinner/>
+  } else {
+    return <Spinner />
   }
-  
 }
 
 export default UserResults
